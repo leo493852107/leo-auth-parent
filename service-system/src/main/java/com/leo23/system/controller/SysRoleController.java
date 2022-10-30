ed94b2d161c6b3f3843b0b5d7b7b3825862361c1
leo <leo493852107@163.com>
2022-10-30 13:46:26 +0800

    角色管理模块-条件分页查询

--- service-system/src/main/java/com/leo23/system/controller/SysRoleController.java
+++ service-system/src/main/java/com/leo23/system/controller/SysRoleController.java
@@ -1,7 +1,10 @@
 package com.leo23.system.controller;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.leo23.common.result.Result;
 import com.leo23.model.system.SysRole;
+import com.leo23.model.vo.SysRoleQueryVo;
 import com.leo23.system.service.SysRoleService;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
@@ -32,5 +35,13 @@ public class SysRoleController {
             return Result.ok();
         }
         return Result.fail();
+    }
+
+    @ApiOperation("条件分页查询")
+    @GetMapping("/{page}/{limit}")
+    public Result findPageQueryRole(@PathVariable Long page, @PathVariable Long limit, SysRoleQueryVo sysRoleQueryVo) {
+        Page<SysRole> pageParam = new Page<>(page, limit);
+        IPage<SysRole> pageModel = sysRoleService.selectPage(pageParam, sysRoleQueryVo);
+        return Result.ok(pageModel);
     }
 }
--- service-system/src/main/java/com/leo23/system/service/impl/SysRoleServiceImpl.java
+++ service-system/src/main/java/com/leo23/system/service/impl/SysRoleServiceImpl.java
@@ -1,11 +1,19 @@
 package com.leo23.system.service.impl;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.leo23.model.system.SysRole;
+import com.leo23.model.vo.SysRoleQueryVo;
 import com.leo23.system.mapper.SysRoleMapper;
 import com.leo23.system.service.SysRoleService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class SysRoleServiceImpl extends ServiceImpl<SysRoleMapper, SysRole> implements SysRoleService {
+    @Override
+    public IPage<SysRole> selectPage(Page<SysRole> pageParam, SysRoleQueryVo sysRoleQueryVo) {
+        IPage<SysRole> pageModel = baseMapper.selectPage(pageParam, sysRoleQueryVo);
+        return pageModel;
+    }
 }
