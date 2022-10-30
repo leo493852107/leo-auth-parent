483496458a730d561ec3901ebd5fbb3ff36c8716
leo <leo493852107@163.com>
2022-10-30 12:33:24 +0800

    统一返回结果

---
+++ common/common-util/src/main/java/com/leo23/common/result/Result.java
@@ -0,0 +1,63 @@
+package com.leo23.common.result;
+
+import lombok.Data;
+
+@Data
+public class Result<T> {
+    private Integer code;
+    private String message;
+    private T data;
+
+    public Result() {
+    }
+
+    public static <T> Result<T> build(T data) {
+        Result<T> result = new Result<T>();
+        if (data != null) {
+            result.setData(data);
+        }
+        return result;
+    }
+
+    public static <T> Result<T> build(T body, Integer code, String message) {
+        Result<T> result = build(body);
+        result.setCode(code);
+        result.setMessage(message);
+        return result;
+    }
+
+    public static <T> Result<T> build(T body, ResultCodeEnum resultCodeEnum) {
+        Result<T> result = build(body);
+        result.setCode(resultCodeEnum.getCode());
+        result.setMessage(resultCodeEnum.getMessage());
+        return result;
+    }
+
+    public static <T> Result<T> ok() {
+        return Result.ok(null);
+    }
+
+    public static <T> Result<T> ok(T data) {
+        Result<T> result = build(data);
+        return build(data, ResultCodeEnum.SUCCESS);
+    }
+
+    public static <T> Result<T> fail() {
+        return Result.fail(null);
+    }
+
+    public static <T> Result<T> fail(T data) {
+        Result<T> result = build(data);
+        return build(data, ResultCodeEnum.FAIL);
+    }
+
+    public Result<T> message(String message) {
+        this.setMessage(message);
+        return this;
+    }
+
+    public Result<T> code(Integer code) {
+        this.setCode(code);
+        return this;
+    }
+}
--- service-system/src/main/java/com/leo23/system/controller/SysRoleController.java
+++ service-system/src/main/java/com/leo23/system/controller/SysRoleController.java
@@ -1,5 +1,6 @@
 package com.leo23.system.controller;
 
+import com.leo23.common.result.Result;
 import com.leo23.model.system.SysRole;
 import com.leo23.system.service.SysRoleService;
 import io.swagger.annotations.Api;
@@ -18,15 +19,18 @@ public class SysRoleController {
 
     @ApiOperation("查询所有角色")
     @GetMapping("/list")
-    public List<SysRole> list() {
+    public Result list() {
         List<SysRole> list = sysRoleService.list();
-        return list;
+        return Result.ok(list);
     }
 
     @ApiOperation("逻辑删除接口")
     @DeleteMapping("/{id}")
-    public boolean delete(@PathVariable Long id) {
+    public Result delete(@PathVariable Long id) {
         boolean b = sysRoleService.removeById(id);
-        return b;
+        if (b) {
+            return Result.ok();
+        }
+        return Result.fail();
     }
 }
